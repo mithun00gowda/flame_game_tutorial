d2869eb27cd19d873519e9749a28f59e363bc7df
Mithun M. <mithun00gowda@gmail.com>
2026-02-19 18:47:46 +0530

    some of the example code of flameGame engine using Flutter

--- lib/src/collision/myCollisionGame.dart
+++ lib/src/collision/myCollisionGame.dart
@@ -122,6 +122,7 @@ class Player extends PositionComponent
     }
   }
 
+  //make the player jump when the screen is tapped
   @override
   void onTapDown(TapDownEvent event) {
     // Reverse horizontal direction
